4eb58206c671d7b932a0efb82668473a4f9e83f2
Krzysztof Betlej <kbetlej@qb-10.136.208.243.cubiware.com>
2018-09-20 16:18:56 +0200

    Init commit: simple espresso test on hello app

---
+++ hello-espresso/app/src/main/java/com/sqisland/android/espresso/hello/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.sqisland.android.espresso.hello
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var greetingView: TextView
+  private lateinit var greetButton: View
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    greetingView = findViewById(R.id.greeting)
+    greetButton = findViewById(R.id.greet_button)
+  }
+
+  fun greet(v: View) {
+    greetButton.isEnabled = false
+    greetingView.setText(R.string.hello)
+  }
+}
